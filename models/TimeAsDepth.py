a1a6d110ac8fe4c754dd8179064167aeb6a3faba
chavicoski <allochi@prhlt.upv.es>
2020-07-25 10:17:09 +0200

    Added model TimeAsDepth4 and logs

--- models/TimeAsDepth.py
+++ models/TimeAsDepth.py
@@ -210,3 +210,57 @@ class TimeAsDepth_3(nn.Module):
         return x
 
 
+class TimeAsDepth_4(nn.Module):
+    '''
+    Convolutional model that takes as input a tensor of shape (batch_size, timesteps, H, W)
+    and uses the 'timesteps' dimension as the channels to employ 2D convolutions. The model
+    outputs a single value to make regresion with the systole or diastole value.
+    '''
+    def __init__(self, in_channels=30):
+        super(TimeAsDepth_4, self).__init__()
+        # Define the model architecture
+        self.conv_block = nn.Sequential(
+                nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=0),
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.Conv2d(64, 64, kernel_size=3, padding=0),
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.Dropout(0.4),
+                nn.Conv2d(64, 128, kernel_size=3, padding=0),
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+                nn.Conv2d(128, 128, kernel_size=3, padding=0),
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+                nn.Dropout(0.4),
+                nn.MaxPool2d(2),
+                nn.Conv2d(128, 256, kernel_size=3, padding=0),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+                nn.Conv2d(256, 256, kernel_size=3, padding=0),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                nn.Dropout(0.4),
+                nn.MaxPool2d(2),
+                nn.Conv2d(256, 512, kernel_size=3, padding=0),
+                nn.BatchNorm2d(512),
+                nn.ReLU(),
+                nn.AdaptiveAvgPool2d((1, 1))
+                )
+        self.flatten = Flatten()
+        self.dense_block = nn.Sequential(
+                nn.Linear(512, 1024),
+                nn.ReLU(),
+                nn.Dropout(0.4),
+                nn.Linear(1024, 1)
+                )
+
+    def forward(self, x):
+        x = self.conv_block(x)
+        x = self.flatten(x)
+        x = self.dense_block(x)
+        return x
+
+
--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from lib.data_generators import Cardiac_dataset
 from lib.utils import *
-from models.TimeAsDepth import TimeAsDepth_0, TimeAsDepth_1, TimeAsDepth_2, TimeAsDepth_3
+from models.TimeAsDepth import TimeAsDepth_0, TimeAsDepth_1, TimeAsDepth_2, TimeAsDepth_3, TimeAsDepth_4
 from models.WideResNet import WideResNet50_0
 
 #######################
@@ -28,7 +28,7 @@ arg_parser.add_argument("--multi_gpu", help="Use all the available GPU's for tra
 arg_parser.add_argument("--pin_mem", help="To use pinned memory for data loading into GPU", type=bool, default=True)
 arg_parser.add_argument("--tensorboard", help="To enable tensorboard logs", type=bool, default=True)
 arg_parser.add_argument("-m", "--model", help="Select the model to train", type=str, 
-        choices=["TimeAsDepth_0", "TimeAsDepth_1", "TimeAsDepth_2", "TimeAsDepth_3"], default="TimeAsDepth_0")
+        choices=["TimeAsDepth_0", "TimeAsDepth_1", "TimeAsDepth_2", "TimeAsDepth_3", "TimeAsDepth_4"], default="TimeAsDepth_0")
 arg_parser.add_argument("-opt", "--optimizer", help="Select the training optimizer", type=str, choices=["Adam", "SGD"], default="Adam")
 arg_parser.add_argument("-lr", "--learning_rate", help="Starting learning rate for the optimizer", type=float, default=0.001)
 arg_parser.add_argument("-da", "--data_augmentation", help="Enable data augmentation", choices=[0, 1, 2], type=int, default=0)
@@ -102,6 +102,8 @@ elif model_name == "TimeAsDepth_2":
     model = TimeAsDepth_2()
 elif model_name == "TimeAsDepth_3":
     model = TimeAsDepth_3()
+elif model_name == "TimeAsDepth_4":
+    model = TimeAsDepth_4()
 elif model_name == "WideResNet50_0":
     model = WideResNet50_0()
     is_pretrained = True
