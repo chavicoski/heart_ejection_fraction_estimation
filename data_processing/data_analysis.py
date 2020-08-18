33853f4d09d323ad7ea72da4e6beec96a35dac6c
chavicoski <alvaro.lopez.chilet@gmail.com>
2020-08-18 21:41:50 +0200

    Improved data_analysis script with more CH stats

--- data_processing/data_analysis.py
+++ data_processing/data_analysis.py
@@ -42,17 +42,27 @@ Info:
 - In the case of having more than 30 frames, we take the last 30 of them that belongs to the same adquisition. Because
   the cases with more than 30 have several adquisitions so the number of frames are a multiple of 30.
 '''
+data_splits    = [("train", train_data_path), ("dev", dev_data_path), ("test", test_data_path)]
+systole_count  = {"train": [], "dev": [], "test": []}  # List with float values for systole labels
+diastole_count = {"train": [], "dev": [], "test": []}  # List with float values for diastole labels
+# SAX 
 pixels_stats   = {"train": [], "dev": [], "test": []}  # Tuples of pixels values (avg, max, min)
 slices_count   = {"train": [], "dev": [], "test": []}  # Number of slices per patient
-two_ch_count   = {"train": [], "dev": [], "test": []}  # Number of 2ch slices per patient
-four_ch_count  = {"train": [], "dev": [], "test": []}  # Number of 4ch slices per patient
-miss_ch_count  = {"train": [], "dev": [], "test": []}  # Available CH slices for the patients that miss at least one of them
 frames_count   = {"train": [], "dev": [], "test": []}  # Number of frames per slice 
 spacing_count  = {"train": [], "dev": [], "test": []}  # Tuples with spaces in each dimension (h, w, depth)
 shapes_count   = {"train": {}, "dev": {}, "test": {}}  # To store the different shapes in the data -> key: shape tuple string, value: number of adquisitions
-systole_count  = {"train": [], "dev": [], "test": []}  # List with float values for systole labels
-diastole_count = {"train": [], "dev": [], "test": []}  # List with float values for diastole labels
-data_splits    = [("train", train_data_path), ("dev", dev_data_path), ("test", test_data_path)]
+# CH
+two_ch_count   = {"train": [], "dev": [], "test": []}  # Number of 2ch slices per patient
+four_ch_count  = {"train": [], "dev": [], "test": []}  # Number of 4ch slices per patient
+miss_ch_count  = {"train": [], "dev": [], "test": []}  # Available CH slices for the patients that miss at least one of them
+frames_count_2ch = {"train": [], "dev": [], "test": []}  # Number of frames per slice 
+frames_count_4ch = {"train": [], "dev": [], "test": []}  # Number of frames per slice 
+spacing_count_2ch  = {"train": [], "dev": [], "test": []}  # Tuples with spaces in each dimension (h, w, depth)
+spacing_count_4ch  = {"train": [], "dev": [], "test": []}  # Tuples with spaces in each dimension (h, w, depth)
+shapes_count_2ch   = {"train": {}, "dev": {}, "test": {}}  # To store the different shapes in the data -> key: shape tuple string, value: number of adquisitions
+shapes_count_4ch   = {"train": {}, "dev": {}, "test": {}}  # To store the different shapes in the data -> key: shape tuple string, value: number of adquisitions
+pixels_stats_2ch   = {"train": [], "dev": [], "test": []}  # Tuples of pixels values (avg, max, min)
+pixels_stats_4ch   = {"train": [], "dev": [], "test": []}  # Tuples of pixels values (avg, max, min)
 
 for split_name, data_path in data_splits:
     for patient in tqdm(os.listdir(data_path), desc=f"{split_name + ' split':<13}"):
@@ -60,18 +70,95 @@ for split_name, data_path in data_splits:
         slices_count[split_name].append(len(sax_dirs))  # To count the number of slices per patient
 
         # Get the number of auxiliary views (2ch and 4ch)
-        two_ch_slices = glob.glob(os.path.join(data_path, patient, "study/2ch_*"))  
-        four_ch_slices = glob.glob(os.path.join(data_path, patient, "study/4ch_*"))
-        two_ch_count[split_name].append(len(two_ch_slices))
-        four_ch_count[split_name].append(len(four_ch_slices))
+        two_ch_dirs = glob.glob(os.path.join(data_path, patient, "study/2ch_*"))  
+        four_ch_dirs = glob.glob(os.path.join(data_path, patient, "study/4ch_*"))
+        two_ch_count[split_name].append(len(two_ch_dirs))
+        four_ch_count[split_name].append(len(four_ch_dirs))
 
-        if len(two_ch_slices) == 0 and len(four_ch_slices) > 0:
+        has_2ch, has_4ch = False, False
+        if len(two_ch_dirs) == 0 and len(four_ch_dirs) > 0:
             miss_ch_count[split_name].append(1)  # 1 for missing 2CH
-        elif len(two_ch_slices) > 0 and len(four_ch_slices) == 0:
+            has_4ch = True
+        elif len(two_ch_dirs) > 0 and len(four_ch_dirs) == 0:
             miss_ch_count[split_name].append(2)  # 2 for missing 4CH
-        elif len(two_ch_slices) == 0 and len(four_ch_slices) == 0:
+            has_2ch = True
+        elif len(two_ch_dirs) == 0 and len(four_ch_dirs) == 0:
             miss_ch_count[split_name].append(3)  # 3 for missing 2CH and 4CH
+        else:
+            has_2ch = True
+            has_4ch = True
 
+        '''Analyze 2CH views'''
+        for two_ch_dir in two_ch_dirs:
+            # Store the original number of frames
+            n_frames = len([f_name for f_name in os.listdir(two_ch_dir) if f_name.endswith(".dcm")])
+            frames_count_2ch[split_name].append(n_frames)
+
+            dicom_files = get_dicom_files(two_ch_dir)  # Get the list of valid dicom files in the 2ch folder
+
+            first = True  # Take some data just from the first frame of the slice
+
+            for dicom_name in dicom_files:
+                if dicom_name == "<BLACK>":
+                    ''' This is a black synthetic slice created for the slices
+                    with less than 30 frames in the slive video'''
+                    continue 
+                else:
+                    dicom_data = dcmread(os.path.join(two_ch_dir, dicom_name)) 
+                    pixel_array = dicom_data.pixel_array
+                    if first:
+                        first = False
+                        # Get the spacing between pixels to scale the images
+                        x_dist, y_dist = dicom_data.PixelSpacing  # Row and column spacing
+                        x_dist, y_dist = float(x_dist), float(y_dist)
+                        z_dist = float(dicom_data.SliceThickness)  # Depth spacing
+                        spacing_count_2ch[split_name].append((x_dist, y_dist, z_dist))
+                        # Store slices shape
+                        img_shape = pixel_array.shape
+                        shapes_count_2ch[split_name][str(img_shape)] = shapes_count[split_name].get(str(img_shape), 0) + 1  
+
+                    # Get pixel values stats
+                    pix_avg = np.mean(pixel_array)
+                    pix_max = np.max(pixel_array)
+                    pix_min = np.min(pixel_array)
+                    pixels_stats_2ch[split_name].append((pix_avg, pix_max, pix_min))
+
+        '''Analyze 4CH views'''
+        for four_ch_dir in four_ch_dirs:
+            # Store the original number of frames
+            n_frames = len([f_name for f_name in os.listdir(four_ch_dir) if f_name.endswith(".dcm")])
+            frames_count_4ch[split_name].append(n_frames)
+
+            dicom_files = get_dicom_files(four_ch_dir)  # Get the list of valid dicom files in the 2ch folder
+
+            first = True  # Take some data just from the first frame of the slice
+
+            for dicom_name in dicom_files:
+                if dicom_name == "<BLACK>":
+                    ''' This is a black synthetic slice created for the slices
+                    with less than 30 frames in the slive video'''
+                    continue 
+                else:
+                    dicom_data = dcmread(os.path.join(four_ch_dir, dicom_name)) 
+                    pixel_array = dicom_data.pixel_array
+                    if first:
+                        first = False
+                        # Get the spacing between pixels to scale the images
+                        x_dist, y_dist = dicom_data.PixelSpacing  # Row and column spacing
+                        x_dist, y_dist = float(x_dist), float(y_dist)
+                        z_dist = float(dicom_data.SliceThickness)  # Depth spacing
+                        spacing_count_4ch[split_name].append((x_dist, y_dist, z_dist))
+                        # Store slices shape
+                        img_shape = pixel_array.shape
+                        shapes_count_4ch[split_name][str(img_shape)] = shapes_count[split_name].get(str(img_shape), 0) + 1  
+
+                    # Get pixel values stats
+                    pix_avg = np.mean(pixel_array)
+                    pix_max = np.max(pixel_array)
+                    pix_min = np.min(pixel_array)
+                    pixels_stats_4ch[split_name].append((pix_avg, pix_max, pix_min))
+
+        '''Analyze SAX views'''
         for sax_dir in sax_dirs:
             # Store the original number of frames
             n_frames = len([f_name for f_name in os.listdir(sax_dir) if f_name.endswith(".dcm")])
@@ -167,7 +254,7 @@ plt.legend(loc="upper left")
 plt.xlabel("N slices")
 plt.ylabel("Count (log scale)")
 plt.title(f"Count of 2CH views by patient for each partition")
-plt.yscale('log', nonposy='clip')
+plt.yscale('log', nonpositive='clip')
 plt.savefig(os.path.join(out_path, f"log_2ch_count.png"))
 plt.clf()  # Reset figure for next plot
 
@@ -194,7 +281,7 @@ plt.legend(loc="upper left")
 plt.xlabel("N slices")
 plt.ylabel("Count (log scale)")
 plt.title(f"Count of 4CH views by patient for each partition")
-plt.yscale('log', nonposy='clip')
+plt.yscale('log', nonpositive='clip')
 plt.savefig(os.path.join(out_path, f"log_4ch_count.png"))
 plt.clf()  # Reset figure for next plot
 
@@ -210,7 +297,8 @@ plt.clf()  # Reset figure for next plot
 '''
 Frames stats
 '''
-print("\nFrames count by slice:")
+
+print("\nSAX frames count by slice:")
 for split, counts in frames_count.items():
     print(f"{split}: mean={mean(counts):.2f}, median={median(counts):.2f}, mode={mode(counts):.2f}, max={max(counts)}, min={min(counts)}")
 
@@ -228,18 +316,106 @@ plt.hist([x[1] for x in frames_count.items()], bins=20, label=[x[0] for x in fra
 plt.legend(loc="upper right")
 plt.xlabel("N frames")
 plt.ylabel("Count (log scale)")
-plt.title(f"Count of frames by slice for each partition")
-plt.yscale('log', nonposy='clip')
+plt.title(f"Count of frames by SAX slice for each partition")
+plt.yscale('log', nonpositive='clip')
 plt.savefig(os.path.join(out_path, f"log_frames_count.png"))
+plt.clf()  # Reset figure for next plot
+
+print("\n2CH frames count by slice:")
+for split, counts in frames_count_2ch.items():
+    print(f"{split}: mean={mean(counts):.2f}, median={median(counts):.2f}, mode={mode(counts):.2f}, max={max(counts)}, min={min(counts)}")
+
+# Histogram of frame counts
+plt.hist([x[1] for x in frames_count_2ch.items()], bins=20, label=[x[0] for x in frames_count_2ch.items()])
+plt.legend(loc="upper right")
+plt.xlabel("N frames")
+plt.ylabel("Count")
+plt.title(f"Count of frames by 2CH slice for each partition")
+plt.savefig(os.path.join(out_path, f"frames_count_2ch.png"))
+plt.clf()  # Reset figure for next plot
+
+# Histogram of frame counts (log scale)
+plt.hist([x[1] for x in frames_count_2ch.items()], bins=20, label=[x[0] for x in frames_count_2ch.items()])
+plt.legend(loc="upper right")
+plt.xlabel("N frames")
+plt.ylabel("Count (log scale)")
+plt.title(f"Count of frames by 2CH slice for each partition")
+plt.yscale('log', nonpositive='clip')
+plt.savefig(os.path.join(out_path, f"log_frames_count_2ch.png"))
+plt.clf()  # Reset figure for next plot
+
+print("\n4CH frames count by slice:")
+for split, counts in frames_count_4ch.items():
+    print(f"{split}: mean={mean(counts):.2f}, median={median(counts):.2f}, mode={mode(counts):.2f}, max={max(counts)}, min={min(counts)}")
+
+# Histogram of frame counts
+plt.hist([x[1] for x in frames_count_4ch.items()], bins=20, label=[x[0] for x in frames_count_4ch.items()])
+plt.legend(loc="upper right")
+plt.xlabel("N frames")
+plt.ylabel("Count")
+plt.title(f"Count of frames by 4CH slice for each partition")
+plt.savefig(os.path.join(out_path, f"frames_count_4ch.png"))
+plt.clf()  # Reset figure for next plot
+
+# Histogram of frame counts (log scale)
+plt.hist([x[1] for x in frames_count_4ch.items()], bins=20, label=[x[0] for x in frames_count_4ch.items()])
+plt.legend(loc="upper right")
+plt.xlabel("N frames")
+plt.ylabel("Count (log scale)")
+plt.title(f"Count of frames by 4CH slice for each partition")
+plt.yscale('log', nonpositive='clip')
+plt.savefig(os.path.join(out_path, f"log_frames_count_4ch.png"))
 plt.clf()  # Reset figure for next plot
 
 '''
 Shapes stats
 '''
-print("\nShapes:")
+print("\nSAX shapes:")
 for split, shapes in shapes_count.items():
     print(f"{split} split:")
     for shape, count in sorted(shapes.items(), key=lambda x: x[1], reverse=True):
+        print(f"\t{shape}: {count:>4}")
+
+print("For all the splits:")
+# Merge splits dicts
+all_shapes_count = {}
+for split, shapes in shapes_count.items():
+    for shape, count in shapes.items():
+        all_shapes_count[shape] = all_shapes_count.get(shape, 0) + count
+# Print the shapes count
+for shape, count in sorted(all_shapes_count.items(), key=lambda x: x[1], reverse=True):
+        print(f"\t{shape}: {count:>4}")
+
+print("\n2CH shapes:")
+for split, shapes in shapes_count_2ch.items():
+    print(f"{split} split:")
+    for shape, count in sorted(shapes.items(), key=lambda x: x[1], reverse=True):
+        print(f"\t{shape}: {count:>4}")
+
+print("For all the splits:")
+# Merge splits dicts
+all_shapes_count_2ch = {}
+for split, shapes in shapes_count_2ch.items():
+    for shape, count in shapes.items():
+        all_shapes_count_2ch[shape] = all_shapes_count_2ch.get(shape, 0) + count
+# Print the shapes count
+for shape, count in sorted(all_shapes_count_2ch.items(), key=lambda x: x[1], reverse=True):
+        print(f"\t{shape}: {count:>4}")
+
+print("\n4CH shapes:")
+for split, shapes in shapes_count_4ch.items():
+    print(f"{split} split:")
+    for shape, count in sorted(shapes.items(), key=lambda x: x[1], reverse=True):
+        print(f"\t{shape}: {count:>4}")
+
+print("For all the splits:")
+# Merge splits dicts
+all_shapes_count_4ch = {}
+for split, shapes in shapes_count_4ch.items():
+    for shape, count in shapes.items():
+        all_shapes_count_4ch[shape] = all_shapes_count_4ch.get(shape, 0) + count
+# Print the shapes count
+for shape, count in sorted(all_shapes_count_4ch.items(), key=lambda x: x[1], reverse=True):
         print(f"\t{shape}: {count:>4}")
 
 '''
